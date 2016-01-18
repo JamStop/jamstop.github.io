cf3cc6a97f3f0175e238027c63e9b988517b3df3
JamStop <jimmyyue1@gmail.com>
2016-01-18 15:33:12 -0800

    gallery implementations

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,12 @@
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        alert('hello');
-    }
-}
+
+
+
+
+// document.onreadystatechange = function () {
+//     if (document.readyState == "complete") {
+//         alert('hello');
+//     }
+// }
 
 // JQUERY VERSION
 // $ ( document ).ready(function() {
